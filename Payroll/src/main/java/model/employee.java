6951cd6a4c31c6b3fa633487d46c5a879d22656b
salina <gotamesalina6@gmail.com>
2025-11-25 13:22:42 +0545

    Add Payroll directory as normal folder

--- Payroll/src/main/java/model/employee.java
+++ Payroll/src/main/java/model/employee.java
@@ -1,8 +1,32 @@
 package model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
+@Table(name="Employee")
+
+@Getter
+@Setter
+    @EqualsAndHashCode
 
 public class employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name ="first_name",nullable=false,length=50)
+    private String firstName;
+    @Column(name = "last_name",nullable=false,length=50)
+    private String lastName;
+    @Column(name="salary",nullable=false,length=50)
+    private String salary;
+
+    @OneToOne(cascade = CascadeType.ALL,mappedBy = "employee")
+    List<payroll> payrollList;
 }
